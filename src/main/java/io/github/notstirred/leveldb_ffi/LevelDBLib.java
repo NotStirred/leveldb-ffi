6876a51b73c4096ee0769cc32c9aa75bbc813d65
Tom Martin <tom.martin1239@gmail.com>
2026-07-09 06:47:01 +0100

    Create api to request loading of packaged libraries

---
+++ src/main/java/io/github/notstirred/leveldb_ffi/LevelDBLib.java
@@ -0,0 +1,55 @@
+package io.github.notstirred.leveldb_ffi;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class LevelDBLib {
+    private static boolean attempted;
+    private static boolean is_success;
+
+    private static final String[] LIBRARIES = {
+            "leveldb_ffi",
+    };
+
+    public static boolean init() {
+        if (attempted) {
+            return is_success;
+        }
+        attempted = true;
+
+        boolean success = false;
+        try {
+            Path tempDir = Files.createTempDirectory("leveldb-ffi-natives");
+            for (String library : LIBRARIES) {
+                String libResource = "natives/" + SystemInfo.OS.osName + "/" + SystemInfo.ARCH.archName + "/" + SystemInfo.OS.getLibraryName(library);
+                Path libFile = tempDir.resolve(SystemInfo.OS.getLibraryName(library)).toAbsolutePath();
+
+                try (InputStream in = LevelDBLib.class.getResourceAsStream(libResource)) {
+                    assert in != null;
+                    Files.copy(in, libFile);
+                }
+                System.load(libFile.toString());
+                deleteIgnoreFail(libFile);
+            }
+            success = true;
+
+            deleteIgnoreFail(tempDir);
+        } catch (Throwable t) {
+            System.err.println("Failed to load leveldb native libraries.");
+            t.printStackTrace(System.err);
+        } finally {
+            is_success = success;
+        }
+
+        return is_success;
+    }
+
+    private static void deleteIgnoreFail(Path libFile) {
+        try {
+            Files.delete(libFile);
+        } catch (Throwable _) {
+            // ignored
+        }
+    }
+}
---
+++ src/main/java/io/github/notstirred/leveldb_ffi/SystemInfo.java
@@ -0,0 +1,94 @@
+package io.github.notstirred.leveldb_ffi;
+
+public class SystemInfo {
+    public static final Architecture ARCH;
+    public static final OperatingSystem OS;
+
+    static {
+
+        String arch = System.getProperty("os.arch", "").toLowerCase().replaceAll("[^a-z0-9]+", "");
+        switch (arch) {
+            case "x64":
+            case "x86_64":
+            case "amd64":
+            case "em64t":
+            case "universal":
+                ARCH = Architecture.x86_64;
+                break;
+            case "x86":
+            case "x86_32":
+            case "x32":
+            case "i386":
+            case "i486":
+            case "i586":
+            case "i686":
+            case "pentium":
+                ARCH = Architecture.x86;
+                break;
+            case "arm":
+            case "arm32":
+                ARCH = Architecture.ARM;
+                break;
+            case "aarch64":
+                ARCH = Architecture.AARCH64;
+                break;
+            default:
+                ARCH = Architecture.UNKNOWN;
+        }
+
+        String osName = System.getProperty("os.name", "").toLowerCase();
+        if (osName.contains("linux")) {
+            OS = OperatingSystem.Linux;
+        } else if (osName.contains("freebsd")) {
+            OS = OperatingSystem.FreeBSD;
+        } else if (osName.contains("openbsd")) {
+            OS = OperatingSystem.OpenBSD;
+        } else if (osName.contains("netbsd")) {
+            OS = OperatingSystem.NetBSD;
+        } else if (osName.contains("windows")) {
+            OS = OperatingSystem.Windows;
+        } else if (osName.contains("mac os")) {
+            OS = OperatingSystem.MAC;
+        } else {
+            OS = OperatingSystem.UNKNOWN;
+        }
+    }
+
+    public enum OperatingSystem {
+        Linux("linux", "lib", ".so"),
+        FreeBSD("freebsd", "lib", ".so"),
+        OpenBSD("openbsd", "lib", ".so"),
+        NetBSD("netbsd", "lib", ".so"),
+        Windows("windows", "", ".dll"),
+        MAC("mac", "", ".dylib"),
+        UNKNOWN("", "", "");
+
+        public final String osName;
+        public final String libPrefix;
+        public final String libSuffix;
+
+        OperatingSystem(String name, String libPrefix, String libSuffix) {
+            this.osName = name;
+            this.libPrefix = libPrefix;
+            this.libSuffix = libSuffix;
+        }
+
+        public String getLibraryName(String name) {
+            return this.libPrefix + name + this.libSuffix;
+        }
+    }
+
+    public enum Architecture {
+        x86("x86"),
+        x86_64("x86_64"),
+        ARM("arm"),
+        AARCH64("aarch64"),
+        UNKNOWN("");
+
+        public final String archName;
+
+        Architecture(String name) {
+            this.archName = name;
+        }
+    }
+}
